159dc3dff27ea49fa9e487f769cf62bb868e37e8
nk-on <2nkoz@proton.me>
2025-03-07 22:55:11 -0800

    Style:adding border to the porfile picture when hovering to it

---
+++ src/Header/Cart.tsx
@@ -0,0 +1,10 @@
+function Cart() {
+  return (
+    <div className=" bg-[#fff] shadow-deep absolute lg:left-[72%] lg:top-[17%] right-[1%] top-[18%] lg:w-[360px] w-[100%] h-[256px]">
+      <div>
+        <h1>Cart</h1>
+      </div>
+    </div>
+  );
+}
+export default Cart;
