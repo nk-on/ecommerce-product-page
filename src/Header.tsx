56276e7aae76bb6e3c1f0b246ea0a168cb5a6e07
nk-on <2nkoz@proton.me>
2025-03-07 21:37:53 -0800

    Style:adding and styling profile picture and cart icon

--- src/Header.tsx
+++ src/Header.tsx
@@ -2,18 +2,32 @@ function Header() {
   const menuItems = ["Collections", "Men", "Women", "About", "Contact"];
   return (
     <>
-      <div className="flex items-center lg:w-[90%] w-full h-[50px] mt-[70px] pb-[30px] border-[#E4E9F2] border-b">
-        <nav className="flex w-[60%] gap-[30px] px-[10px]">
-          <img src="public/icon-menu.svg" alt="menu-icon" className="lg:hidden block" />
-          <img src="public/logo.svg" alt="sneakers" className="w-[137.5px] h-[20px]" />
-          <ul className="lg:flex justify-between w-[60%] hidden">
-            {menuItems.map((item) => (
-              <li className="text-[#69707D] text-[15px] cursor-pointer border-[#FF7E1B]  hover:border-b-[4px]">
-                 <div className="pb-[50px]">{item}</div>
-              </li>
-            ))}
-          </ul>
-        </nav>
+      <div className="flex justify-between  lg:w-[90%] w-full h-[50px] mt-[70px] pb-[30px] border-[#E4E9F2] border-b">
+        <div>
+          <nav className="flex w-[90%] gap-[30px] px-[10px]">
+            <img src="public/icon-menu.svg" alt="menu-icon" className="lg:hidden block" />
+            <img src="public/logo.svg" alt="sneakers" className="w-[137.5px] h-[20px]" />
+            <ul className="lg:flex justify-between w-[60%] gap-[30px] hidden">
+              {menuItems.map((item) => (
+                <li className="text-[#69707D] text-[15px] cursor-pointer border-[#FF7E1B]  hover:border-b-[4px]">
+                  <div className="pb-[20px]">{item}</div>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </div>
+        <div className="flex justify-between items-center lg:w-[9%] w-[15%] h-full">
+        <img
+            src="public/icon-cart.svg"
+            alt="icon-cart"
+            className="w-[21.82px] h-[20px] cursor-pointer"
+          />
+          <img
+            src="public/image-avatar.png"
+            alt="image-avatar"
+            className="lg:w-[50px] lg:h-[50px] w-[24px] h-[24px] cursor-pointer"
+          />
+        </div>
       </div>
     </>
   );
