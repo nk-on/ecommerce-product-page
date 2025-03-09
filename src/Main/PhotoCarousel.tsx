54c046047117f541760dc43798d5ac1941af8595
nk-on <2nkoz@proton.me>
2025-03-08 21:58:34 -0800

    Feat:displaying thumbnail image as a main image if user clicks it

--- src/Main/PhotoCarousel.tsx
+++ src/Main/PhotoCarousel.tsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
+
 interface photoProps {
   url: string;
   urlThumbNail: string;
+  setMainImage: React.Dispatch<React.SetStateAction<string>>;
 }
 const photos = [
   {
@@ -24,22 +27,32 @@ const photos = [
     photoThumbnail: "public/image-product-4-thumbnail.jpg",
   },
 ];
-function Photo({ url, urlThumbNail }: photoProps) {
+function Photo({ url, urlThumbNail, setMainImage }: photoProps) {
   return (
     <div
-      className={` w-[88px] h-[88px] cursor-pointer rounded-[10px]`}
+      className={` w-[88px] h-[88px] cursor-pointer rounded-[10px] hover:gradient-overlay`}
       style={{ background: `url(${urlThumbNail})`, backgroundSize: "cover" }}
+      onClick={() => setMainImage(url)}
     ></div>
   );
 }
 function PhotoCarousel() {
+  const [mainImage, setMainImage] = useState<string>("public/image-product-1.jpg");
   return (
     <>
       <div className=" flex flex-col gap-[20px] lg:mt-[50px] lg:ml-[40px] lg:w-[445px] lg:h-[445px]  ">
-        <div className="bg-[url(public/image-product-1.jpg)] bg-cover bg-no-repeat lg:w-[100%] lg:h-[140%]  h-[300px] w-[100%] lg:rounded-[15px]"></div>
+        <div
+          className="lg:w-[100%] lg:h-[140%]  h-[300px] w-[100%] lg:rounded-[15px]"
+          style={{ backgroundImage: `url(${mainImage})`, backgroundSize: "cover", backgroundPosition:'center' }}
+        ></div>
         <div className="lg:flex justify-between hidden ">
           {photos.map((photo) => (
-            <Photo url={photo.photo} urlThumbNail={photo.photoThumbnail} key={photo.id} />
+            <Photo
+              url={photo.photo}
+              urlThumbNail={photo.photoThumbnail}
+              setMainImage={setMainImage}
+              key={photo.id}
+            />
           ))}
         </div>
       </div>
