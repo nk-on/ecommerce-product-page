04a7dda5e120c01d68bbd77a506c339e60c54ebd
nk-on <2nkoz@proton.me>
2025-03-08 22:15:35 -0800

    Adding photo carousel component to the App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Header from './Header'
+import MainPage from './MainPage'
 function App() {
 
   return (
@@ -7,6 +8,7 @@ function App() {
      <div className='flex justify-center'>
        <Header />
      </div>
+     <MainPage />
     </>
   )
 }
