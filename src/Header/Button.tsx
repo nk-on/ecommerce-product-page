cf051e0cd115d9f48c86a5752febddc12ea7b5f1
nk-on <2nkoz@proton.me>
2025-03-10 21:44:00 -0700

    Fix:fixing positioning of the prev/next buttons

--- src/Header/Button.tsx
+++ src/Header/Button.tsx
@@ -5,7 +5,7 @@ interface ButtonProps {
 }
 function Button({ url,top,left }:ButtonProps) {
   return (
-    <div className={`lg:w-[56px] lg:h-[56px] w-[40px] h-[40px] bg-[#FFFF] absolute left-[${left}%] top-[${top}%]   lg:hidden  rounded-[50%] flex justify-center items-center cursor-pointer`}>
+    <div className={`lg:w-[56px] lg:h-[56px] w-[40px] h-[40px] bg-[#FFFF] absolute left-[${left}%] top-[${top}%] bottom-[50%] ${left === 0 && 'right-0'} translate-y-[50%] lg:hidden  rounded-[50%] flex justify-center items-center cursor-pointer`}>
       <img src={url}></img>
     </div>
   );
--- src/Main/Buttons.ts
+++ src/Main/Buttons.ts
@@ -1,13 +1,13 @@
 const buttons = [
     {
         id:0,
-        url:"public/icon-next.svg",
+        url:" public/icon-previous.svg",
         left:90,
         top:50,
     },
     {
         id:1,
-        url:"public/icon-previous.svg",
+        url:"public/icon-next.svg ",
         left:0,
         top:50,
     }
