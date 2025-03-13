22ffe4105d2f111878d9149ebc05dfda98740828
nk-on <2nkoz@proton.me>
2025-03-13 15:25:50 -0700

    Style:adding hover effect on close button when user hovers on

--- src/Main/Overlay.tsx
+++ src/Main/Overlay.tsx
@@ -1,16 +1,25 @@
 import photos from "./photos";
 import Photo from "./Photo";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import OverlayContext from "../Context";
 interface OverLayProps {
   mainImage: string;
   setMainImage: React.Dispatch<React.SetStateAction<string>>;
 }
 function OverLay({ mainImage, setMainImage }: OverLayProps) {
-  const {setExpanded} = useContext(OverlayContext)
+  const { setExpanded } = useContext(OverlayContext);
+  const [hovered, setHovered] = useState<boolean>(false);
   return (
     <div className="hidden fixed w-full h-full top-0 left-0 right-0 bottom-0 bg-black bg-opacity-50 z-20 cursor-pointer lg:flex flex-col justify-center items-center">
-      <img src="public/icon-close.svg" className="ml-[37%]" onClick={()=> setExpanded(false)}></img>
+      <div
+        className=' w-[20px] h-[20px] ml-[35%]'
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
+        onClick={()=> setExpanded(true)}
+        style={{
+          backgroundImage:hovered ? 'url(public/IconClose-hovered.svg)' : 'url(public/iconClose.svg)'
+        }}
+      ></div>
       <img src={`${mainImage}`} className="w-[550px] h-[550px] rounded-[15px]"></img>
       <div className="lg:flex justify-between hidden ">
         {photos.map((photo) => (
