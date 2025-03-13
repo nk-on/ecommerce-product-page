37751193fefd48e5e02485817435bbcad273c763
nk-on <2nkoz@proton.me>
2025-03-12 21:41:05 -0700

    Adjusting positions of button

--- src/Header/Button.tsx
+++ src/Header/Button.tsx
@@ -1,13 +1,17 @@
 interface ButtonProps {
-  url:string,
-  top:number,
-  left:number
+  url: string;
+  top: number;
+  left: number;
 }
-function Button({ url,top,left }:ButtonProps) {
+function Button({ url, top, left }: ButtonProps) {
   return (
-    <div className={`lg:w-[56px] lg:h-[56px] w-[40px] h-[40px] bg-[#FFFF] absolute left-[${left}%] top-[${top}%] bottom-[50%] ${left === 0 && 'right-0'} translate-y-[50%] lg:hidden  rounded-[50%] flex justify-center items-center cursor-pointer`}>
+    <div
+      className={`lg:w-[56px] lg:h-[56px] w-[40px] h-[40px] bg-[#FFFF] absolute left-[${left}%] top-[${top}%] bottom-[50%] ${
+        left === 0 && "right-0"
+      } translate-y-[50%] lg:hidden  rounded-[50%] flex justify-center items-center cursor-pointer`}
+    >
       <img src={url}></img>
     </div>
   );
-};
+}
 export default Button;
