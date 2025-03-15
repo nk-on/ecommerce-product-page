7f54150d0828d4673fce13c0e1b6ec8102ba141e
nk-on <2nkoz@proton.me>
2025-03-15 10:58:33 -0700

    Fixing image path of cart icon

--- src/Header.tsx
+++ src/Header.tsx
@@ -21,7 +21,7 @@ function Header() {
         </div>
         <div className="flex justify-between items-center lg:w-[9%] w-[15%] h-full">
         <img
-            src="public/icon-cart.svg"
+            src="public/Cart.svg"
             alt="icon-cart"
             className="w-[21.82px] h-[20px] cursor-pointer"
             onClick={()=> setProfilePicClicked(prev => !prev)}
