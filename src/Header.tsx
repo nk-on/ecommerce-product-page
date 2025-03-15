a83babd4a8abd4079bd977fbc769b17b068e674e
nk-on <2nkoz@proton.me>
2025-03-15 11:15:28 -0700

    Adding hover to the button

--- src/Header.tsx
+++ src/Header.tsx
@@ -21,7 +21,7 @@ function Header() {
         </div>
         <div className="flex justify-between items-center lg:w-[9%] w-[15%] h-full">
         <img
-            src="public/Cart.svg"
+            src="public/Cart-dark.svg"
             alt="icon-cart"
             className="w-[21.82px] h-[20px] cursor-pointer"
             onClick={()=> setProfilePicClicked(prev => !prev)}
--- src/ProductData.tsx
+++ src/ProductData.tsx
@@ -13,15 +13,28 @@ function ProductData() {
             <h1 className="font-bold text-[28px]">125.00$</h1>
             <p className="line-through text-[#B6BCC8]">$250.00</p>
           </div>
-          <div className="bg-[#FFEEE2] w-[51px] h-[27px] rounded-[6px] text-[#FF7E1B] flex justify-center items-center">50%</div>
+          <div className="bg-[#FFEEE2] w-[51px] h-[27px] rounded-[6px] text-[#FF7E1B] flex justify-center items-center">
+            50%
+          </div>
         </div>
         <div className="flex gap-[5px]">
-            <div className="w-[157px] h-[56px] bg-[#F6F8FD] rounded-[10px] flex justify-around items-center">
-                <img src="public/icon-minus.svg" alt="minus" className="w-[12px] h-[3.33px]"></img>
-                <div>0</div>
-                <img src="public/icon-plus.svg" alt="+"  className="w-[12px] h-[13.71px]"/>
-            </div>
-            <button className="w-[272px] h-[56px] bg-[#FF7E1B] rounded-[10px] flex justify-around items-center"><img src="public/Cart.svg"></img>Add to cart</button>
+          <div className="w-[157px] h-[56px] bg-[#F6F8FD] rounded-[10px] flex justify-around items-center">
+            <img
+              src="public/icon-minus.svg"
+              alt="minus"
+              className="w-[12px] h-[3.33px] cursor-pointer"
+            ></img>
+            <div>0</div>
+            <img
+              src="public/icon-plus.svg"
+              alt="+"
+              className="w-[12px] h-[13.71px] cursor-pointer"
+            />
+          </div>
+          <button className="w-[272px] h-[56px] bg-[#FF7E1B] rounded-[10px] flex justify-around items-center hover:bg-[#FFAB6A]">
+            <img src="public/Cart.svg"></img>{" "}
+            <div className="text-[#FFFF]">Add to cart</div>
+          </button>
         </div>
       </div>
     </>
